2d679aed075e09dfbe2b713588e03cb136f063cb
Dan Nuffer <danielnuffer@gmail.com>
2014-03-03 22:34:54 -0700

    Refactored to clean things and up be able to control iterations better.

--- src/opt/test/MaximumAreaTest.java
+++ src/opt/test/MaximumAreaTest.java
@@ -11,12 +11,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import org.joda.time.DateTime;
 import org.joda.time.Duration;
 
 import dist.DiscreteDependencyTree;
 import dist.DiscretePermutationDistribution;
 import dist.DiscreteUniformDistribution;
 import dist.Distribution;
+import opt.EvaluationFunction;
 import opt.OptimizationAlgorithm;
 import opt.RandomizedHillClimbingWithRestart;
 import opt.SwapNeighbor;
@@ -56,14 +58,35 @@ public class MaximumAreaTest {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-
+		List<Problem> problems = new ArrayList<Problem>();
 		for (int p = 1; p <= NUM_PROBLEMS; p++) {
-			String problemName = "MaximumAreaTest_" + p * 10;
-			String problemDataFilename = problemName + ".csv";
-			double[][] points = loadProblem(problemDataFilename);
-			System.out.println("Loaded " + problemDataFilename + " " + points.length);
-			doProblem(points, problemName);
+			problems.add(loadProblem(p));
 		}
+		
+
+		// now do a run for the iteration limit
+		System.out.println("JIT PRIMING AND COUNTING ITERATIONS");
+		ArrayList<TrainResults> results = doRunAll(new ProblemRun(problems.get(0), null, null, 0, new TimeLimit(Duration.standardSeconds(MaximumAreaTest.SECONDS))));
+		int[] iterationLimits = new int[results.size()];
+		for (int i = 0; i < results.size(); i++) {
+			iterationLimits[i] = results.get(i).iterations;
+			//System.out.println("iterationLimits[" + i + "]: " + iterationLimits[i]);
+		}
+		System.out.println("DONE JIT PRIMING AND COUNTING ITERATIONS");
+		
+		
+		for (Problem problem : problems) {
+			doProblem(problem, iterationLimits);
+		}
+	}
+
+	private static Problem loadProblem(int p) throws IOException {
+		String problemName = "MaximumAreaTest_" + p * 10;
+		String problemDataFilename = problemName + ".csv";
+		double[][] points = loadProblemData(problemDataFilename);
+		System.out.println("Loaded " + problemDataFilename + " " + points.length);
+		Problem problem = new Problem(points, problemName);
+		return problem;
 	}
 
 	static double[] toDoubleArray(List<Double> list) {
@@ -82,7 +105,7 @@ public class MaximumAreaTest {
 		return ret;
 	}
 
-	private static double[][] loadProblem(String problemDataFilename) throws IOException {
+	private static double[][] loadProblemData(String problemDataFilename) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(new File(problemDataFilename)));
 		try {
 			ArrayList<List<Double>> results = new ArrayList<List<Double>>();
@@ -105,60 +128,302 @@ public class MaximumAreaTest {
 		}
 	}
 
-	private static void doProblem(double[][] points, String problemName) throws FileNotFoundException {
-		int N = points.length;
 
-		PrintWriter results_csv = new PrintWriter(problemName + "_results.csv");
-		results_csv.print("Algorithm,Run,BestFitness,Time,Iterations,BestIteration,SecondsPerIteration\n");
-		PrintWriter traces_output = new PrintWriter("MaximumAreaTest_" + N + "_traces.csv");
+	public static class Problem {
+		public double[][] points;
+		public String problemName;
+		public int n;
+
+		public Problem(double[][] points, String problemName) {
+			this.points = points;
+			this.problemName = problemName;
+			this.n = points.length;
+		}
+		
+		public EvaluationFunction getEvaluationFunction() {
+			return new MaximumAreaEvaluationFunction(points, n / 2);
+		}
+		
+		public Distribution getDistribution() {
+			return new DiscretePermutationDistribution(n);
+		}
+	}
+
+	public static class ProblemRun {
+		public Problem problem;
+		public ResultHandler resultHandler;
+		public PrintWriter traces_output;
+		public int run;
+		public TrainingTerminationCondition termination;
+
+		public ProblemRun(Problem p, ResultHandler resultHandler, PrintWriter traces_output, int run, TrainingTerminationCondition termination) {
+			this.problem = p;
+			this.resultHandler = resultHandler;
+			this.traces_output = traces_output;
+			this.run = run;
+			this.termination = termination;
+		}
+	}
+	
+	public static interface TrainingTerminationCondition {
+		public void start();
+		public boolean shouldContinue(ParameterizedAlgorithmRun run); 
+	}
+	
+	public static class TimeLimit implements TrainingTerminationCondition {
+		private Duration timeLimit;
+		private DateTime end;
+		public TimeLimit(Duration timeLimit) {
+			this.timeLimit = timeLimit;
+		}
+		@Override
+		public void start() {
+			end = DateTime.now().plus(timeLimit);
+		}
+		@Override
+		public boolean shouldContinue(ParameterizedAlgorithmRun run) {
+			return end.isAfterNow();
+		}
+	}
+	
+	public static class MultiRunIterationLimit implements TrainingTerminationCondition {
+		private int[] iterationLimits;
+		private int run = -1;
+		private int iteration = 0;
+		public MultiRunIterationLimit(int[] iterationLimits) {
+			this.iterationLimits = iterationLimits;
+		}
+		@Override
+		public void start() {
+			run = (run + 1) % iterationLimits.length;
+			iteration = 0;
+		}
+		@Override
+		public boolean shouldContinue(ParameterizedAlgorithmRun algRun) {
+			iteration++;
+			return iteration < iterationLimits[run];
+		}
+		
+	}
+	
+	public static interface ResultHandler {
+		public void handle(String shortName, int run, double optimalFitness, double seconds, int iterations, int bestIteration);
+	}
+	
+	public static class ResultCSVWriter implements ResultHandler {
+		PrintWriter results_csv;
+		public ResultCSVWriter(PrintWriter results_csv) {
+			this.results_csv = results_csv;
+			results_csv.print("Algorithm,Run,BestFitness,Time,Iterations,BestIteration,SecondsPerIteration\n");
+		}
+		public void handle(String shortName, int run, double optimalFitness, double seconds, int iterations, int bestIteration) {
+			results_csv.print(shortName + "," + run + "," + optimalFitness + "," + seconds + "," + iterations + ","
+					+ bestIteration + "," + seconds / iterations + "\n");
+		}
+		public void close() {
+			results_csv.close();
+		}
+	}
+
+
+	private static void doProblem(Problem problem, int[] iterationLimits) throws FileNotFoundException {		
+		ResultCSVWriter results_csv = new ResultCSVWriter(new PrintWriter(problem.problemName + "_results.csv"));
+		PrintWriter traces_output = new PrintWriter("MaximumAreaTest_" + problem.n + "_traces.csv");
     	traces_output.print("Algorithm,Run,TraceIdx,Iteration,BestFitness,CurrentFitness\n");
 
-    	// for rhc, sa, and ga we use a permutation based encoding
-		MaximumAreaEvaluationFunction ef = new MaximumAreaEvaluationFunction(points, N / 2);
-		Distribution odd = new DiscretePermutationDistribution(N);
-
 		for (int run = 0; run < NUM_RUNS; run++) {
-			doTrainAndRecord(run, results_csv, ef, traces_output, new RandomizedHillClimbing(makeHCP(ef, odd)));
-
-			doTrainAndRecord(run, results_csv, ef, traces_output, new RandomizedHillClimbingWithRestart(makeHCP(ef, odd), 6000));
-			doTrainAndRecord(run, results_csv, ef, traces_output, new SimulatedAnnealing(1E12, .95, makeHCP(ef, odd)));
-			doTrainAndRecord(run, results_csv, ef, traces_output, new SimulatedAnnealing(1E12, .99, makeHCP(ef, odd)));
-
-			doTrainAndRecord(run, results_csv, ef, traces_output, new StandardGeneticAlgorithm(200, 150, 20, makeGAP(N, ef, odd)));
-			doTrainAndRecord(run, results_csv, ef, traces_output, new StandardGeneticAlgorithm(200, 180, 10, makeGAP(N, ef, odd)));
-			doTrainAndRecord(run, results_csv, ef, traces_output, new MIMIC(200, 100, createMIMICPOP(N, ef)));
-			doTrainAndRecord(run, results_csv, ef, traces_output, new MIMIC(300, 50, createMIMICPOP(N, ef)));
+			// TODO: use iteration count from first run here
+			doRunAll(new ProblemRun(problem, results_csv, traces_output, run, new MultiRunIterationLimit(iterationLimits)));
 		}
 		results_csv.close();
 		traces_output.close();
 	}
+	
+	public static class ParameterizedAlgorithmRun extends OptimizationAlgorithm {
+		ParameterizedAlgorithm pa;
+		ProblemRun pr;
+		Tracer tracer;
+		public ParameterizedAlgorithmRun(ParameterizedAlgorithm pa, ProblemRun pr) {
+			super(pa.oa.getOptimizationProblem());
+			this.pa = pa;
+			this.pr = pr;
+			this.tracer = makeTracer(); // call this last
+		}
+		
+		public String getShortName() {
+			return pa.oa.getShortName();
+		}
 
-	private static void doTrainAndRecord(int run, PrintWriter results_csv, MaximumAreaEvaluationFunction ef, PrintWriter traces_output, 
-			OptimizationAlgorithm optAlg) throws FileNotFoundException {
-		Tracer tracer = new PeriodicTraceCSVLogger(optAlg.getShortName() + "," + run, Duration.millis(SECONDS * 1000 / 100), traces_output);
+		@Override
+		public double train() {
+			return pa.oa.train();
+		}
 
-		double start = System.nanoTime();
-		TrainResults results = doTrain(ef, optAlg, tracer);
-		double end = System.nanoTime();
-		double time = (end - start) / 10e9;
-		results_csv.print(optAlg.getShortName() + "," + run + "," + results.optimalFitness + "," + time + "," + results.iterations + ","
-				+ results.bestIteration + "," + time / results.iterations + "\n");
+		@Override
+		public Instance getOptimal() {
+			return pa.oa.getOptimal();
+		}
+
+		@Override
+		public String getDescription() {
+			return pa.oa.getDescription();
+		}
+
+		public double getOptimalValue() {
+			return pa.ef.value(pa.oa.getOptimal());
+		}
+
+		private Tracer makeTracer() {
+			if (pr.traces_output != null) {
+				return new PeriodicTraceCSVLogger(getShortName() + "," + pr.run, Duration.millis(SECONDS * 1000 / 100), pr.traces_output);
+			} else {
+				return null;
+			}
+		}
+
+		void recordRun(MaximumAreaTest.TrainResults results, double time) {
+			if (pr.resultHandler != null) {
+				pr.resultHandler.handle(getShortName(), pr.run, results.optimalFitness, time, results.iterations, results.bestIteration);
+			}
+		}
+
+		TrainResults doTrain() {
+			System.out.println("Starting " + getDescription());
+//			TimeLimitTrainer fit = new TimeLimitTrainer(new OccasionalPrinter(this, Duration.millis(200), getOptimizationProblem()), Duration.standardSeconds(MaximumAreaTest.SECONDS), 
+//					makeTracer());
+//			double finalFitness = fit.train();
+			
+			int iterations = 0;
+			double value = Double.MAX_VALUE;
+			int bestIteration = 0;
+			double best = Double.MIN_VALUE;
+			
+			pr.termination.start();
+			
+			while (pr.termination.shouldContinue(this)) {
+				iterations++;
+				value = train();
+				if (tracer != null) {
+					tracer.trace(iterations, getOptimalValue(), value);
+				}
+				if (value > best) {
+					best = value;
+					bestIteration = iterations;
+				}
+			}
+
+			double finalFitness = value;
+			
+			System.out.println(getDescription() + " optimal: " + getOptimalValue() + " final: " + finalFitness);
+			System.out.println("Best iteration: " + bestIteration);
+			return new MaximumAreaTest.TrainResults(finalFitness, getOptimalValue(), iterations, bestIteration);
+		}
+
+		TrainResults doTrainAndRecord() throws FileNotFoundException {
+			double start = System.nanoTime();
+			MaximumAreaTest.TrainResults results = doTrain();
+			double end = System.nanoTime();
+			double time = (end - start) / 10e9;
+			recordRun(results, time);
+			return results;
+		}
+		
+	}
+	
+	public static class ParameterizedAlgorithm extends OptimizationAlgorithm {
+		EvaluationFunction ef;
+		OptimizationAlgorithm oa;
+		Distribution odd;
+		
+		public ParameterizedAlgorithm(EvaluationFunction ef, OptimizationAlgorithm oa, Distribution odd) {
+			super(oa.getOptimizationProblem());
+			this.ef = ef;
+			this.oa = oa;
+			this.odd = odd;
+		}
+
+		public String getShortName() {
+			return oa.getShortName();
+		}
+
+		@Override
+		public double train() {
+			return oa.train();
+		}
+
+		@Override
+		public Instance getOptimal() {
+			return oa.getOptimal();
+		}
+
+		@Override
+		public String getDescription() {
+			return oa.getDescription();
+		}
+
+		public double getOptimalValue() {
+			return ef.value(oa.getOptimal());
+		}
 	}
 
-	private static HillClimbingProblem makeHCP(MaximumAreaEvaluationFunction ef, Distribution odd) {
+	private static ArrayList<TrainResults> doRunAll(ProblemRun runParameters)
+			throws FileNotFoundException {
+		ArrayList<TrainResults> results = new ArrayList<TrainResults>();
+		results.add(makeHCPRun(runParameters).doTrainAndRecord());
+		results.add(makeHCPWRRun(runParameters, 6000).doTrainAndRecord());
+		results.add(makeSARun(runParameters, 1E12, .95).doTrainAndRecord());
+		results.add(makeSARun(runParameters, 1E12, .99).doTrainAndRecord());
+		results.add(makeSGARun(runParameters, 200, 150, 20).doTrainAndRecord());
+		results.add(makeSGARun(runParameters, 200, 180, 10).doTrainAndRecord());
+		results.add(makeMIMICRun(runParameters, 200, 100).doTrainAndRecord());
+		results.add(makeMIMICRun(runParameters, 300, 50).doTrainAndRecord());
+		return results;
+	}
+
+	private static ParameterizedAlgorithmRun makeHCPRun(ProblemRun runParameters) {
+		EvaluationFunction ef = runParameters.problem.getEvaluationFunction();
+		Distribution odd = runParameters.problem.getDistribution();
+		return new ParameterizedAlgorithmRun(new ParameterizedAlgorithm(ef, new RandomizedHillClimbing(makeHCP(ef,  odd)), odd), runParameters);
+	}
+
+	private static ParameterizedAlgorithmRun makeHCPWRRun(ProblemRun runParameters, int restartInterval) {
+		EvaluationFunction ef = runParameters.problem.getEvaluationFunction();
+		Distribution odd = runParameters.problem.getDistribution();
+		return new ParameterizedAlgorithmRun(new ParameterizedAlgorithm(ef, new RandomizedHillClimbingWithRestart(makeHCP(ef,  odd), restartInterval), odd), runParameters);
+	}
+
+	private static ParameterizedAlgorithmRun makeSARun(ProblemRun runParameters, double t, double cooling) {
+		EvaluationFunction ef = runParameters.problem.getEvaluationFunction();
+		Distribution odd = runParameters.problem.getDistribution();
+		return new ParameterizedAlgorithmRun(new ParameterizedAlgorithm(ef, new SimulatedAnnealing(t, cooling, makeHCP(ef,  odd)), odd), runParameters);
+	}
+
+	private static ParameterizedAlgorithmRun makeSGARun(ProblemRun runParameters, int populationSize, int toMate, int toMutate) {
+		EvaluationFunction ef = runParameters.problem.getEvaluationFunction();
+		Distribution odd = runParameters.problem.getDistribution();
+		return new ParameterizedAlgorithmRun(new ParameterizedAlgorithm(ef, new StandardGeneticAlgorithm(populationSize, toMate, toMutate, makeGAP(runParameters.problem.n, ef,  odd)), odd), runParameters);
+	}
+
+	private static ParameterizedAlgorithmRun makeMIMICRun(ProblemRun runParameters, int samples, int toKeep) {
+		EvaluationFunction ef = runParameters.problem.getEvaluationFunction();
+		Distribution odd = runParameters.problem.getDistribution();
+		return new ParameterizedAlgorithmRun(new ParameterizedAlgorithm(ef, new MIMIC(samples, toKeep, createMIMICPOP(runParameters.problem.n, ef)), odd), runParameters);
+	}
+
+	private static HillClimbingProblem makeHCP(EvaluationFunction ef, Distribution odd) {
 		NeighborFunction nf = new SwapNeighbor();
 		HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
 		return hcp;
 	}
 
-	private static GeneticAlgorithmProblem makeGAP(int N, MaximumAreaEvaluationFunction ef, Distribution odd) {
+	private static GeneticAlgorithmProblem makeGAP(int N, EvaluationFunction ef, Distribution odd) {
 		MutationFunction mf = new SwapMutation();
 		CrossoverFunction cf = new OrderedCrossOver(N);
 		GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);
 		return gap;
 	}
 
-	private static ProbabilisticOptimizationProblem createMIMICPOP(int N, MaximumAreaEvaluationFunction ef) {
+	private static ProbabilisticOptimizationProblem createMIMICPOP(int N, EvaluationFunction ef) {
 		Distribution odd;
 		int[] ranges = new int[N];
 		Arrays.fill(ranges, N);
@@ -181,14 +446,5 @@ public class MaximumAreaTest {
 		public double optimalFitness;
 		public int iterations;
 		public int bestIteration;
-	}
-
-	private static TrainResults doTrain(MaximumAreaEvaluationFunction ef, OptimizationAlgorithm oa, Tracer tracer) {
-		System.out.println("Starting " + oa.getDescription());
-		TimeLimitTrainer fit = new TimeLimitTrainer(new OccasionalPrinter(oa, Duration.millis(200), oa.getOptimizationProblem()), Duration.standardSeconds(SECONDS), tracer);
-		double finalFitness = fit.train();
-		System.out.println(oa.getDescription() + " optimal: " + ef.value(oa.getOptimal()) + " final: " + finalFitness);
-		System.out.println("Best iteration: " + fit.getBestIteration());
-		return new TrainResults(finalFitness, ef.value(oa.getOptimal()), fit.getIterations(), fit.getBestIteration());
 	}
 }
